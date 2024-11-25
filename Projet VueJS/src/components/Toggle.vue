25a96752a312c3e51bd48d7a7689e6f00e76b7fe
Mouadeb-Sofiane <116799161+Mouadeb-Sofiane@users.noreply.github.com>
2024-11-25 15:00:11 +0100

    feat(toggle) : ajout d'un toggle avec ecrit True et False

---
+++ Projet VueJS/src/components/Toggle.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+
+// État du bouton bascule (true = activé, false = désactivé)
+const isActive = ref(false);
+
+// Fonction pour basculer l'état
+const toggleState = () => {
+  isActive.value = !isActive.value;
+  console.log("Toggle State:", isActive.value ? "true" : "false");
+};
+</script>
+
+<template>
+    <div>
+      <div
+        :class="['toggle', isActive ? 'true' : 'false']"
+        @click="toggleState"
+      >
+        <div class="track -font"></div>
+        <div class="thumb -font"></div>
+      </div>
+      
+      <!-- Affichage du texte True ou False -->
+      <p>{{ isActive ? 'True' : 'False' }}</p>
+    </div>
+</template>
