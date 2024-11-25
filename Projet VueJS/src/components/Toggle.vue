579aa06177087ad4bcc550d5c94f27f70f072f5b
Mouadeb-Sofiane <116799161+Mouadeb-Sofiane@users.noreply.github.com>
2024-11-25 15:03:19 +0100

    toggle scss

--- Projet VueJS/src/components/Toggle.vue
+++ Projet VueJS/src/components/Toggle.vue
@@ -1,10 +1,8 @@
 <script lang="ts" setup>
 import { ref } from "vue";
 
-// État du bouton bascule (true = activé, false = désactivé)
 const isActive = ref(false);
 
-// Fonction pour basculer l'état
 const toggleState = () => {
   isActive.value = !isActive.value;
   console.log("Toggle State:", isActive.value ? "true" : "false");
@@ -21,7 +19,56 @@ const toggleState = () => {
         <div class="thumb -font"></div>
       </div>
       
-      <!-- Affichage du texte True ou False -->
       <p>{{ isActive ? 'True' : 'False' }}</p>
     </div>
 </template>
+  
+<style lang="scss">
+.toggle {
+  cursor: pointer;
+  width: 48px;
+  height: 24px;
+  position: relative;
+
+  .track {
+    width: 100%;
+    height: 100%;
+    border-radius: 12px;
+    transition: background-color 0.3s ease;
+  }
+
+  .thumb {
+    position: absolute;
+    top: 6px;
+    width: 12px;
+    height: 12px;
+    border-radius: 50%;
+    background-color: $thumb-color;
+    transition: left 0.3s ease;
+  }
+
+  &.false {
+    .track {
+      background-color: $color-false;
+      .-font {
+        font-family: $fontTextFamily;
+      }
+    }
+    .thumb {
+      left: 6px;
+      .-font {
+        font-family: $fontTextFamily;
+      }
+    }
+  }
+
+  &.true {
+    .track {
+      background-color: $color-true;
+    }
+    .thumb {
+      left: 30px;
+    }
+  }
+}
+</style>
