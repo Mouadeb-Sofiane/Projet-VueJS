41618f2c725e6750e7bb94c94cf32352d8179193
Mouadeb-Sofiane <116799161+Mouadeb-Sofiane@users.noreply.github.com>
2024-12-02 13:54:01 +0100

    feat(layout) : nuxt layout initialisation

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="l-default">
+        <Header />
+        <slot />
+        <Footer />
+    </div>
+</template>
+
+<style lang="scss"></style>
---
+++ layouts/minimal.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="l-minimal">
+       <slot />
+    </div>
+</template>
+
+<style lang="scss"></style>
