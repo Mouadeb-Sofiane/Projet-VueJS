fb8d6aa36dc4089553897095433e1be4d596a9fd
Mouadeb-Sofiane <116799161+Mouadeb-Sofiane@users.noreply.github.com>
2024-12-03 09:42:53 +0100

    feat(nuxt) : mise en place de sanity

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', "@nuxtjs/sanity"],
 
   css : ['~/assets/scss/main.scss'],
+  
+  sanity: {
+    projectId: "a8btv06f",
+    dataset: "production",
+  },
 
   components : [
     {
