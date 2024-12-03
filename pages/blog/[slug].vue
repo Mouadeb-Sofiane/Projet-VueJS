0a24c8b2fc5790370a0aaf4ad37bf8b0b3673284
Mouadeb-Sofiane <116799161+Mouadeb-Sofiane@users.noreply.github.com>
2024-12-03 14:53:38 +0100

    feat(404) :  creation d'une page 404

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -5,14 +5,22 @@ const POST_QUERY = groq`*[_type == "post" && slug.current == $slug][0]`;
 const route = useRoute();
 
 const { data: post } = await useSanityQuery<SanityDocument>(POST_QUERY, { slug: route.params.slug, });
+
+if(!post.value){
+    throw createError({ statusCode: 404, statusMessage: 'Le post est introuvable.' });
+}
+
 </script>
 
 <template>
  <div class="margin" v-if="post">
     une page d'article de blog : {{ post }}
     <h1>{{ post.title }}</h1>
-
+        <div v-if="post.image">
+            <SanityImage :asset-id="post.image.asset._ref" />
+        </div>
     <SanityContent v-bind="{ blocks: post.body }" />
+
  </div>
 </template>
 
